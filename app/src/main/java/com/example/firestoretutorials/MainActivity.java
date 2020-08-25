d36931542967a653d19d0a94113ed87ea7deaa50
iamsss <saurav9760@gmail.com>
2020-08-26 02:44:17 +0530

    Saving First Data To Android Store

--- app/src/main/java/com/example/firestoretutorials/MainActivity.java
+++ app/src/main/java/com/example/firestoretutorials/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.firestoretutorials;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyDebug";
+    private EditText enterTitle;
+    private EditText enterThought;
+    Button saveBtn;
+
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_THOUGHTS = "thought";
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterTitle = findViewById(R.id.editText);
+        enterThought = findViewById(R.id.editThought);
+        saveBtn = findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = enterTitle.getText().toString().trim();
+                String thought = enterThought.getText().toString().trim();
+
+                Map<String,Object> data = new HashMap<>();
+                data.put(KEY_TITLE,title);
+                data.put(KEY_THOUGHTS,thought);
+
+                db.collection("Journal")
+                        .document("First Thoughts")
+                        .set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText( MainActivity.this,"Sucess Saving Data",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG,"onFailure - "+ e.toString());
+                    }
+                });
+            }
+        });
     }
 }
